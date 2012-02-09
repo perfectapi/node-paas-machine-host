9d1aefb1622252f2882b49766fad1bec9e0fc724
Steve Campbell <dukeytoo@gmail.com>
2012-02-09 16:09:42 -0600

    Still a WIP - basic working claim and install

--- bin/host.js
+++ bin/host.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-var perfectapi = require('perfectapi');  
-//var perfectapi = require('../../perfectapi/api.js')
+//var perfectapi = require('perfectapi');  
+var perfectapi = require('../../perfectapi/api.js')
 var path = require('path');
 var main = require('../lib/main.js');
+var claimer = require('../lib/claim.js');
 
 var configPath = path.resolve(__dirname, '..', 'perfectapi.json');
 var parser = new perfectapi.Parser();
@@ -11,19 +12,36 @@ var pollInterval = 60 * 1000;
 
 //handle the commands
 parser.on("publishApp", function(config, callback) {
-  main.publishApp(config, function(err, result) {
-    callback(err, result);
+  var registryEndpoint = config.environment.SERVICE_REGISTRY_URL;
+  if (!registryEndpoint || registryEndpoint === '') return callback('SERVICE_REGISTRY_URL not specified');
+  
+  perfectapi.proxy(registryEndpoint, function(err, registry) {
+    if (err) return callback(err);
+    
+    main.publishApp(registry, config, function(err, result) {
+      callback(err, result);
+    });    
   });
+
 });
 
-parser.on("publishApp", function(config, callback) {
-  main.publishApp(config, function(err, result) {
-    callback(err, result);
-  });
-});
+
 
 parser.on("server", function(config) {
 
+  var registryEndpoint = config.environment.SERVICE_REGISTRY_URL;
+  if (!registryEndpoint || registryEndpoint === '') return console.error('SERVICE_REGISTRY_URL not specified');
+  
+  perfectapi.proxy(registryEndpoint, function(err, registry) {
+    if (err) return console.error(err);
+    
+    setInterval(function() {
+      //TODO: check first if our load is low enough.
+      claimer.claim(config, registry);
+    }, pollInterval);
+    
+  });
+  
 })
 
 module.exports = parser.parse(configPath);
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # What is this?
 
-This is a component of a self-hosted PaaS solution for PerfectAPI services.  It provides a per-machine host, installing and starting service instances
+This is a component of a self-hosted PaaS solution for PerfectAPI services.  It provides a per-machine host, installing and starting service instances.
+
+It also provides a command-line-interface (CLI) to the hosted services.  (You can use it to publish apps).
